384ca39b1ff25d6dac2eec082d4f2941685a8316
Sayrix <xaviercraft99@gmail.com>
2022-11-03 20:43:40 +0100

    test

---
+++ sponsor.config.ts
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { defineConfig, presets } from 'sponsorkit'
+
+export default defineConfig({
+  tiers: [
+    {
+      title: 'Sponsors',
+      // to replace the entire tier rendering
+      // compose: (composer, tierSponsors, config) => {
+      //   composer.addRaw(
+      //     '<-- custom svg -->',
+      //   )
+      // },
+    },
+    {
+      title: 'Silver Sponsors',
+      monthlyDollars: 10,
+      preset: presets.medium,
+      // to insert custom elements after the tier block
+      composeAfter: (composer, tierSponsors, config) => {
+        composer.addSpan(10)
+      },
+    },
+    {
+      title: 'Gold Sponsors',
+      monthlyDollars: 50,
+      preset: presets.large,
+    }
+  ],
+})
